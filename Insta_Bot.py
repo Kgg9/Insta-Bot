1c1e19b59771e78438ce8d34153f7e304ed25c6c
Kgg9 <Kartikeyg910@gmail.com>
2020-08-20 02:33:13 -0400

    got a login in script bruh we on our way

--- Insta_Bot.py
+++ Insta_Bot.py
@@ -1,2 +1,28 @@
-print("Hello World Bruh")
-print("Aight checking if this thing updates Bruh")
+from selenium import webdriver
+from webdriver_manager.chrome import *
+from time import *
+from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
+
+
+# login script
+class login:
+
+    def __init__(self, user, psd):
+        self.user = user
+        self.psd = psd
+
+        self.lscirpt()
+
+    def lscirpt(self):
+        driver.get("https://instagram.com")
+        sleep(1)
+        usearch = driver.find_element_by_name("username").send_keys(self.user)
+        psearch = driver.find_element_by_name('password').send_keys(self.psd)
+
+
+
+
+login('fish_can_drown', 'AwAw!234')
+
