e85a167dfa9e79c5190e3a37f6b95f81af3df7ac
Kgg9 <Kartikeyg910@gmail.com>
2020-10-29 05:25:05 -0400

    Need to working on the auto feed commenter but looks good so far, got the commenting working for one post atleast

--- Insta_Bot.py
+++ Insta_Bot.py
@@ -5,6 +5,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from random import randint
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
 
@@ -32,7 +33,7 @@ class login:
             LC = WebDriverWait(driver, 10).until(
                 EC.element_to_be_clickable((By.XPATH, '//*[@id="react-root"]/section/main/div/div/div/div/button')))
             LC.click()
-        except NameError:
+        except:
             pass
 
     def Nclick(self):
@@ -40,7 +41,7 @@ class login:
             NC = WebDriverWait(driver, 10).until(
                 EC.element_to_be_clickable((By.XPATH, '/html/body/div[4]/div/div/div/div[3]/button[2]')))
             NC.click()
-        except NameError:
+        except:
             pass
 
 
@@ -48,18 +49,36 @@ class login:
 login('fish_can_drown', 'AwAw!234')
 
 class autoFollowSugs:
-    driver.find_element_by_xpath('//*[@id="react-root"]/section/main/section/div[3]/div[2]/div[1]/a/div').click()
-    plcHld = 0
-    fllwBtn = driver.find_elements_by_xpath("//button[contains(.,'Follow')]")
+    def __init__(self):
+        driver.find_element_by_xpath('//*[@id="react-root"]/section/main/section/div[3]/div[2]/div[1]/a/div').click()
+        fllwBtn = driver.find_elements_by_xpath("//button[contains(.,'Follow')]")
 
-    for i in fllwBtn[:5]:
-        driver.execute_script("arguments[0].click();", i)
-        sleep(1)
-
-autoFollowSugs()
+        for i in fllwBtn[:5]:
+            driver.execute_script("arguments[0].click();", i)
+            sleep(1)
 
 
+class fllwListCmnt:
+    def __init__(self, *comments):
+
+        self.comments = list(comments)
+        length = len(self.comments)
+
+        imgAlt = driver.find_elements_by_tag_name("img")
+        fPic = imgAlt[0]
+        lPic = imgAlt[-1]
+
+        for i in imgAlt:
+            while(i!=lPic):
+                driver.find_element_by_class_name("RxpZH").click()
+                sleep(1)
+                cmntBox = driver.find_element_by_css_selector("form textarea").send_keys(self.comments[randint(0, length-1)])
+                sleep(2)
+                driver.find_element_by_xpath("//button[@type='submit']").click()
+        fPic = lPic
+        driver.refresh()
 
 
 
 
+fllwListCmnt("bruh", "okurrrr", "boom")
